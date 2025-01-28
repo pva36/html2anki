1a489646dfe9c9f07127e26f5760656d5eb2c430
Pedro Valdebenito Gutiérrez <154631161+pva36@users.noreply.github.com>
2025-01-28 15:09:07 -0300

    fix .gitignore

--- test_html2anki/test_html_file_to_list.py
+++ test_html2anki/test_html_file_to_list.py
@@ -1,8 +1,13 @@
 import sys
+
 sys.path.append("..")
 
-from html2anki import html_file_to_list, strip_list_of_flashcards, \
-                        list_to_basic_n_cloze, create_html_template
+from html2anki import (
+    html_file_to_list,
+    strip_list_of_flashcards,
+    list_to_basic_n_cloze,
+    create_html_template,
+)
 
 
 def main():
@@ -15,15 +20,18 @@ def main():
     print("\n")
 
     print("stripped_list_of_flashcards(list_of_flashcards, br_switch)")
-    list_of_flashcards_stripped = strip_list_of_flashcards(list_of_flashcards,
-                                                           True)
+    list_of_flashcards_stripped = strip_list_of_flashcards(
+        list_of_flashcards, True
+    )
     for flashcard in list_of_flashcards_stripped:
         print("\n")
         for line in flashcard:
             print(f'"{line}"')
 
     print("list_to_basic_n_cloze(list_of_flashcards)")
-    list_of_basic, list_of_cloze = list_to_basic_n_cloze(list_of_flashcards_stripped)
+    list_of_basic, list_of_cloze = list_to_basic_n_cloze(
+        list_of_flashcards_stripped
+    )
 
     print("\nBASIC:\n")
     for flashcard in list_of_basic:
@@ -34,6 +42,7 @@ def main():
         print(flashcard)
 
     create_html_template("template.html")
+
 
 if __name__ == "__main__":
     main()
